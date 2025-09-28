211d24161d4ef7eea15dcbc40b258e559493479f
Lambitox <mercyosahonibadin@gmail.com>
2025-09-28 20:30:12 +0100

    Add src/App.js for React setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Lambitek Online Academy</h1>
+      <p>Your deployment is working!</p>
+    </div>
+  );
+}
+
+export default App;
